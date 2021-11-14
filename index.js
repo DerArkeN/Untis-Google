693156eb04fb17941f039e1852d76e2d64cd3ac5
tripleX <vodelsup@gmail.com>
2021-11-14 20:00:46 +0100

    Finished new update() function

--- index.js
+++ index.js
@@ -2,14 +2,13 @@ const untis = require('./untis.js');
 const google = require('./google');
 
 (async () => {
-    // const interval = setInterval(async() => {
-    //     let date = new Date();
+    const interval = setInterval(async() => {
+        let date = new Date();
 
-    //     if(date.getHours() == 13 && date.getMinutes() == 0) {
-    //         console.log(date.getHours(), ':' ,date.getMinutes());
-    //         await untis.update();
-    //         console.log(date.getHours(), ':' ,date.getMinutes());
-    //     }
-    // }, 60000);
-    await untis.update(new Date());
+        if(date.getHours() == 6 && date.getMinutes() == 0) {
+            console.log(date.getHours(), ':' ,date.getMinutes());
+            await untis.update();
+            console.log(date.getHours(), ':' ,date.getMinutes());
+        }
+    }, 60000);
 })();
