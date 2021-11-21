d30be8108572163b245b4a92c7b353274f4a9bfa
tripleX <vodelsup@gmail.com>
2021-11-21 17:40:58 +0100

    added running check

--- index.js
+++ index.js
@@ -6,16 +6,24 @@ const logger = require('./logger');
     console.log('Started untis-google.');
 
     let oldT = await untis.getTimetable();
+    let running = false;
     const checkUpdate = setInterval(async() => {
+        if(running) return;
+        running = true;
         let curT = await untis.getTimetable();
 
         //Check if update occured
-        if(JSON.stringify(oldT) === JSON.stringify(curT)) return false;
-        logger.info(`Update received`, {time: `${new Date()}`});
-        //Update events
-        await untis.update();
-        //Check if new update got new events and add them in case
-        if(oldT.length !== curT.length) await untis.addNew(oldT, curT);
-        oldT = curT;
-    }, 60000);
+        if(JSON.stringify(oldT) !== JSON.stringify(curT)) {
+            logger.info(`Update received`, {time: `${new Date()}`});
+            console.log(`Update received`);
+            //Update events
+            await untis.update();
+            //Check if new update got new events and add them in case
+            if(oldT.length !== curT.length) await untis.addNew(oldT, curT);
+            //Update oldT to curT
+            oldT = curT;
+        }
+
+        running = false;
+    }, 60 * 1000);
 })();
